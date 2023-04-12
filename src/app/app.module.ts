0cc9e4d407e514fecf6cc96268ad5c1c65830760
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-12 13:19:28 -0500

    feat: clase 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponenteUnoComponent } from './clase02/componente-uno/componente-uno.component';
+import { ComponenteDosComponent } from './clase02/componente-dos/componente-dos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ComponenteUnoComponent, ComponenteDosComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
