b26dda8acafd87b25d963e8eff8c040f0fb88594
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-13 08:54:45 -0500

    feat: clase 04 - ejemplo directiva personalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { ComponenteUnoComponent } from './clase02/componente-uno/componente-uno.component';
 import { ComponenteDosComponent } from './clase02/componente-dos/componente-dos.component';
 import { InterpolacionComponent } from './clase04/interpolacion/interpolacion.component';
+import { MiDirectivaDirective } from './clase04/mi-directiva.directive';
 
 @NgModule({
-  declarations: [AppComponent, ComponenteUnoComponent, ComponenteDosComponent, InterpolacionComponent],
+  declarations: [AppComponent, ComponenteUnoComponent, ComponenteDosComponent, InterpolacionComponent, MiDirectivaDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
